c91dd7ef76a08903bcc3bfa48fefbc3c120cac2d
Mitali <kavita3.gawai@gmail.com>
2026-08-01 20:56:53 +0530

    fixed config

--- backend/config.py
+++ backend/config.py
@@ -9,5 +9,7 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+if not JWT_SECRET_KEY:
+    raise RuntimeError("JWT_SECRET_KEY env var is not set")
 
     UPLOAD_FOLDER = "uploads"
