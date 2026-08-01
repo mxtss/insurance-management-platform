7dd866bbf2a40e5fae614e077c15fd23055abedb
Mitali <kavita3.gawai@gmail.com>
2026-08-01 20:22:05 +0530

    saved

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://insurance-backend-1-bhfx.onrender.com",
+  baseURL: "https://insurance-backend-1-bhfx.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   }
