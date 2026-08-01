f4583c7b9e75b87be84322e08227d0584b8b6729
Mitali <kavita3.gawai@gmail.com>
2026-08-01 20:19:03 +0530

    fixed somethings

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -2,7 +2,7 @@ import api from "./api";
 
 export const login = async (email, password) => {
   try {
-    const response = await api.post("/auth/login", {
+    const response = await api.post("/api/auth/login", {
       email,
       password,
     });
