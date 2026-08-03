e3e80533b0301009e87e0d1b37798af867e5b752
Mitali <kavita3.gawai@gmail.com>
2026-08-03 16:54:41 +0530

    authServie js

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,43 +1,67 @@
 import api from "./api";
 
 export const login = async (email, password) => {
+
   try {
+
     const response = await api.post("/auth/login", {
       email,
       password,
     });
 
-export const register = async (user) => {
-
-  const response = await api.post(
-    "/auth/register",
-    user
-  );
-
-  return response.data;
-
-};
-
     return response.data;
+
   } catch (error) {
+
     throw (
       error.response?.data || {
         message: "Unable to connect to server.",
       }
     );
+
   }
+
+};
+
+export const register = async (user) => {
+
+  try {
+
+    const response = await api.post(
+      "/auth/register",
+      user
+    );
+
+    return response.data;
+
+  } catch (error) {
+
+    throw (
+      error.response?.data || {
+        message: "Unable to register user.",
+      }
+    );
+
+  }
+
 };
 
 export const getProfile = async () => {
+
   try {
+
     const response = await api.get("/auth/profile");
+
     return response.data;
+
   } catch (error) {
+
     throw (
       error.response?.data || {
         message: "Unable to fetch profile.",
       }
     );
-  }
-};
 
+  }
+
+};
